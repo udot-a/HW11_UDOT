8b8f3705b63e609ae1fd8903110254ad15a1a92a
Udot Andrii <udot.dron@gmail.com>
2019-10-06 11:02:51 +0300

    last ver

--- hw11.js
+++ hw11.js
@@ -1,38 +1,4 @@
 // ************************* REQUIRED ******************************
-const obj = {
-   pictures: [
-        {
-            id: 1,
-            title: "Голлум",
-            src: "https://avatarko.ru/img/kartinka/1/Gollum_Middle-earth.jpg"
-        },
-        {
-            id: 2,
-            title: "Фродо",
-            src: "https://v1.popcornnews.ru/k2/news/970/upload/HBebQ3.jpg"
-        },
-        {
-            id: 3,
-            title: "Арагорн",
-            src: "https://upload.wikimedia.org/wikipedia/ru/thumb/8/87/Ringstrilogyposter.jpg/274px-Ringstrilogyposter.jpg"
-        },
-        {
-            id: 4,
-            title: "Гендольф",
-            src: "https://cs9.pikabu.ru/post_img/2018/02/25/9/1519570465127811508.jpg"
-        },
-        {
-            id: 5,
-            title: "Хоббит",
-            src: "https://u.kanobu.ru/editor/images/66/28ed1902-8d07-4ad2-a4c7-4309b0fd3068.jpg"
-        },
-        {
-            id: 6,
-            title: "Леголас",
-            src: "https://gordonua.com/img/article/1907/96_tn.jpg"
-        }
-    ]
-}
 function printImg(data) {
     elements = []
     Object.values(JSON.parse(data))[0].forEach((item, index) => {
@@ -52,71 +18,61 @@ readFile.onclick = (event) => {
 
     request.onload = function (event) {
         this.status === 200 ?
-            printImg(this.response) : result = null
+            printImg(this.response) : console.warn('Something wrong with loading file!')
     }
     request.send()
 }
 
 // ******************* ADDITIONAL #2 *******************************************
-
-// function User(name, email, photoURL) {
-
-//     name ? this.name = name : this.name = 'Ivan McGregor'
-//     email ? this.email = email : this.email = 'sobaka@sobaka.dog'
-//     photoURL ? this.photoURL = photoURL : this.photoURL = User.getAvatar()
-// }
-
-// User.prototype.messageBox = (function () {
-//     var output = additonalTwo.appendChild(document.createElement('div'))
-//     output.img = output.appendChild(document.createElement('img'))
-//     output.img.style.width="50px"
-//     output.userName = output.appendChild(document.createElement('p'))
-//     output.textMessage = output.appendChild(document.createElement('textarea'))
-//     output.textMessage.oninput = (event) => {
-//         output.img.src = User.admin.photoURL
-//         output.userName.innerText = User.admin.name;
-//     }
-//     return output
-// })()
-// User.avatars = [
-//     "https://whatsism.com/uploads/posts/2018-07/1530544023_n6fgwzftnvg.jpg",
-//     "https://klike.net/uploads/posts/2019-03/1551511862_28.jpg",
-//     "https://www.meme-arsenal.com/memes/7bdea6754f999b50e9577596f09197fb.jpg",
-//     "http://icons.iconarchive.com/icons/hopstarter/face-avatars/256/Male-Face-L3-icon.png",
-//     "https://findicons.com/files/icons/1072/face_avatars/300/i05.png",
-//     "http://www.iconarchive.com/download/i51043/hopstarter/halloween-avatars/Gomez.ico",
-//     "http://icons.iconarchive.com/icons/hopstarter/halloween-avatars/256/Zombie-2-icon.png",
-//     "https://vignette.wikia.nocookie.net/yogscast/images/8/8a/Avatar_Turps_2015.jpg"
+// var messages = [
+//     "backspace",
+//     "enter",
+//     "shift",
+//     "control",
+//     "delete",
+//     "space",
+//     "subtract",
+//     "turn left",
+//     "turn right",
+//     "turn down",
+//     "turn up"
 // ]
-// User.admin = {
-//     photoURL: "https://i.pinimg.com/originals/3d/47/4f/3d474f82ff71595e8081f9a120892ae8.gif",
-//     name: "admin"
+// getKey = () => {
+//     let counter = 1;
+//     let key = new Date().toLocaleTimeString()
+//     for (let i in log) i.split('[')[0] === key && counter++
+//     return counter > 1 ? `${key}[${counter}]` : key
 // }
-// User.getAvatar = function () {
-//     return this.avatars.shift()
-// }
-
-// User.prototype.write = function (message) {
-//     this.messageBox.textMessage.value = message;
-//     this.messageBox.img.src = this.photoURL;
-//     this.messageBox.userName.innerText = this.name;
-// }
-
-// var users = [
-//     new User("Иван"),
-//     new User('Alex', "alex@gmail.com"),
-//     new User('Bob', "bob777@gmail.com"),
-//     new User('Dima', "dima888@gmail.com"),
-//     new User('Fima', "fima999@gmail.com")
-// ]
-
-// var k = 1
-// users.forEach(
-//     function (user) {
-//         setTimeout(
-//             function () {
-//                 user.write(`Hello, I'm ${user.name}`)
-//             }, 3000 * k++
-//         )
-//     }
+// var log = {}
+// var sendMessage = message => new Promise(
+//     resolve => setTimeout(
+//         () => resolve(message),
+//         Math.random() * 7000
+//     )
 // )
+// messages.forEach (
+//     message => {sendMessage ( message)
+//         .then( message =>  
+//             Object.assign(log,{[getKey()]:message}))
+
+//     }
+// ) 
+// var sendAll = () => {
+//     var index = 0
+//     function recursive() {
+//         // debugger
+//         let message = messages[index]
+//         index++ < messages.length ?
+//             sendMessage(message)
+//                 .then(message => {
+//                     Object.assign(log, { [getKey()]: message })
+//                     recursive()
+//                 }) : null
+
+
+//     }
+//     recursive()
+// }
+
+// sendAll()
+
